af665991a2d8d4b78f085674c030b596f8bf26ca
ohmzs <38644134+ohmzs@users.noreply.github.com>
2022-05-17 08:29:27 -0400

    me

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import About from "about";
+import Home from "home";
+import NavBar from "nav";
 function App() {
   return (
     <div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+
 function NavBar() {
   return (
-    <nav>
+    <nav id="nav">
       <a href="#home">I'm a link!</a>
     </nav>
   );
 }
+export default function NavBar();
